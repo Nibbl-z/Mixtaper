9bd36740b85afaa5f842ac1f5af32b6b6d18c7fc
Nibbl-z <thisisnotnibblz123@gmail.com>
2025-01-02 14:18:18 -0700

    feat(frontend): :sparkles: navbar

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+    import Topbar from "../components/topbar.svelte";
+</script>
+
+<Topbar/>
 <h1>Welcome to SvelteKit</h1>
+
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<a href="https://www.flaticon.com/free-icons/search" title="search icons">Search icons created by Chanut - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by Freepik - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/upload" title="upload icons">Upload icons created by Ilham Fitrotul Hayat - Flaticon</a>
